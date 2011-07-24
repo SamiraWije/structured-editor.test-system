e9e5f9568163df0c3c7254fbf74e2340fac515ef
Ilya Posov <iposov@gmail.com>
2011-07-25 00:19:39 +0400

    restructure project

---
+++ TestSystem/src/testSystem/lang/comb/ArrayExpr.java
@@ -0,0 +1,30 @@
+package testSystem.lang.comb;
+
+import ru.ipo.structurededitor.model.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ilya
+ * Date: 05.08.2010
+ * Time: 16:29:20
+ */
+public abstract class ArrayExpr extends Expr {
+    private Expr items[];
+    protected char op;
+    protected boolean vert = false;
+
+    public Expr[] getItems() {
+        return items;
+    }
+
+    public void setItems(Expr items[]) {
+        this.items = items;
+    }
+
+    public Cell getLayout() {
+        if (vert)
+            return new ArrayFieldCell("items", ArrayFieldCell.Orientation.Vertical);
+        else
+            return new Horiz(new ConstantCell("("), new ArrayFieldCell("items", ArrayFieldCell.Orientation.Horizontal), new ConstantCell(")"));
+    }
+}
---
+++ TestSystem/src/testSystem/lang/logic/LogicAtom.java
@@ -0,0 +1,31 @@
+package testSystem.lang.logic;
+
+import ru.ipo.structurededitor.model.Cell;
+import ru.ipo.structurededitor.model.DSLBeanParams;
+import ru.ipo.structurededitor.model.FieldCell;
+import ru.ipo.structurededitor.model.Horiz;
+import testSystem.lang.comb.Expr;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ilya
+ * Date: 05.08.2010
+ * Time: 16:07:49
+ */
+@DSLBeanParams(shortcut = "Cуждение", description = "Простое суждение")
+public class LogicAtom extends Expr {
+
+    private String val;
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    public Cell getLayout() {
+        return new Horiz(new FieldCell("val"));
+    }
+}

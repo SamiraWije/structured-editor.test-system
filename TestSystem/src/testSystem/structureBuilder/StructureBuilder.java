f29ed176e63c42b950d1a73612f1848889227a59
olegperch <olegperch@localhost>
2011-08-23 13:14:50 +0400

    Angle definition by 3 points. Dynamic segment and angle eqaulty.

--- TestSystem/src/testSystem/structureBuilder/StructureBuilder.java
+++ TestSystem/src/testSystem/structureBuilder/StructureBuilder.java
@@ -1,9 +1,7 @@
 package testSystem.structureBuilder;
 
 import geogebra.euclidian.EuclidianView;
-import geogebra.kernel.GeoElement;
-import geogebra.kernel.GeoPoint;
-import geogebra.kernel.GeoSegment;
+import geogebra.kernel.*;
 import geogebra.main.Application;
 import org.w3c.dom.*;
 import org.w3c.dom.Element;
@@ -163,8 +161,38 @@ public class StructureBuilder {
         if (caption.matches("[A-Z][A-Z]")){
             GeoPoint p1 =(GeoPoint)getGeoByCaption(String.valueOf(caption.charAt(0)),app);
             GeoPoint p2 = (GeoPoint)getGeoByCaption(String.valueOf(caption.charAt(1)),app);
-            if (p1!=null&& p2!=null)
-                return new GeoSegment(app.getKernel().getConstruction(),p1,p2);
+            if (p1!=null&& p2!=null){
+                GeoSegment gs = new GeoSegment(app.getKernel().getConstruction(),p1,p2);
+                gs.calcLength();
+                return gs;
+            }
+        } else if (caption.matches("[A-Z][A-Z][A-Z]")){
+            GeoPoint p1 =(GeoPoint)getGeoByCaption(String.valueOf(caption.charAt(0)),app);
+            GeoPoint p2 = (GeoPoint)getGeoByCaption(String.valueOf(caption.charAt(1)),app);
+            GeoPoint p3 = (GeoPoint)getGeoByCaption(String.valueOf(caption.charAt(2)),app);
+
+            if (p1!=null&& p2!=null && p3!=null){
+                GeoAngle gs = new GeoAngle(app.getKernel().getConstruction());
+                double bx, by, vx, vy, wx, wy;
+                bx = p2.inhomX;
+                by = p2.inhomY;
+                vx = p1.inhomX - bx;
+                vy = p1.inhomY - by;
+                wx = p3.inhomX - bx;
+                wy = p3.inhomY - by;
+                if (app.getKernel().isZero(vx) && app.getKernel().isZero(vy) ||
+        		   app.getKernel().isZero(wx) && app.getKernel().isZero(wy)) {
+                    gs.setUndefined();
+        	        return gs;
+                  }
+
+                double det = vx * wy - vy * wx;
+    	        double prod = vx * wx + vy * wy;
+    	        double value = Math.atan2(det, prod);
+                                
+                gs.setValue(value);
+                return gs;
+            }
         }
         for (Object geo : geos) {
             if (geo instanceof GeoElement && ((GeoElement) geo).getCaption().equals(caption)) {
